1c7b56992f9c63ed58a85edeb9a5991256a4ac90
MichaelChen147 <33444924+MichaelChen147@users.noreply.github.com>
2018-07-23 20:04:51 +0800

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,10 @@
+inputs = {"up": [1, -2],
+          "down": [1, 2],
+          "w": [0, -2],
+          "s": [0, 2]}
+
+def keydown(key):
+    for i in inputs:
+        if key == simplegui.KEY_MAP[i]:
+            paddle_vel[inputs[i][0]] += inputs[i][1]
+
