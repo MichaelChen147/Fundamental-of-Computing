362a5c80333c00ecdee3db4c7c4ae7ed57da8edf
MichaelChen147 <33444924+MichaelChen147@users.noreply.github.com>
2018-07-26 19:26:33 +0800

    Update List and Set.py

--- List and Set.py
+++ List and Set.py
@@ -1,7 +1,7 @@
 Set:
 
 # Example1
-
+# set immutable in loop
 instructors = set(['Rixner', 'Warren', 'Greiner', 'Wong'])
 print instructors
 
@@ -16,7 +16,7 @@ get_rid_of(instructors, 'W')
 print instructors
 
 # Example2
-
+# use list() to avoid the situation where set is immutable in loop
 rs = set([])
 for s in myset:
     rs.add(s)
@@ -26,4 +26,8 @@ or
 
 for s in list(myset):
     myset.remove(s)
+    
+#Example3
+# cell[row][col]
+cells = [ [... for dummy_col in range(grid_width)] for dummy_row in range(grid_height)]
 
